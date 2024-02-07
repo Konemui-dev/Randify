d418c0e2eb43a2ff124263c17dfddadd738cf56a
JedGardiner <JGardinerP@gmail.com>
2024-02-07 21:36:36 +0000

    added some basic theme settings for the UI, remove twitch usage because ill branch that ig

---
+++ popupfn.js
@@ -0,0 +1,14 @@
+document.getElementById("ToggleTheme").addEventListener("click",Theme);
+
+function Theme(){
+    document.body.classList.toggle("dark-theme")
+
+  }
+
+  
+
+document.getElementById("ToggleStatus").addEventListener("click",AppStatus);
+//dont work atm lol
+function AppStatus(){
+
+  }
